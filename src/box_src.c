d826030754d06d45b86dc400492b29064bd1de7c
99003788 <78849542+99003788@users.noreply.github.com>
2021-02-22 00:43:16 +0530

    deleted  function.

--- src/box_src.c
+++ src/box_src.c
@@ -224,17 +224,3 @@ void average_vol(struct Node *first)
 }
 
 
-void average_vol(struct Node *first)
-{
-    int sum=0;
-    struct Node *curr;
-    double avg;
-    int i;
-    for(curr = first;curr!=NULL;curr=curr->next)
-    {
-        sum += ((curr->data.length)*(curr->data.breadth)*(curr->data.height));
-        i++;
-    }
-    avg = sum/i;
-    printf("\n Average of all the boxes = %lf",avg);
-}
